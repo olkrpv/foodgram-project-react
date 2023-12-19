4bcae31cd190ec4b10a373999e47bde186c31859
Olga Karpova <tordvist@yandex.ru>
2023-12-20 00:31:33 +0300

    Установлены обязательные поля в модели пользователя и изменен USERNAME_FIELD на email

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
     """Кастомный класс пользователя."""
+
+    email = models.EmailField(_('email address'), unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
